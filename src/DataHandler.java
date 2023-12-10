109de84946bde5f630200b451e2ce7d8a00ab410
Erica Dong <erica3280cell@gmail.com>
2023-12-10 02:35:45 -0500

    Debugged, separated, generated. Thriving.

---
+++ src/DataHandler.java
@@ -0,0 +1,36 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.security.NoSuchAlgorithmException;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class DataHandler {
+    public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
+        encodeData(100000, "./src/data.csv");
+    }
+
+    public static void encodeData(int size, String outFile) throws IOException, NoSuchAlgorithmException {
+        //chance of overlap is so small that it doesn't matter
+        //encoded in hex
+
+        PrintWriter pw = new PrintWriter(new FileWriter(outFile));
+        String[][] data = new String[size][81];
+        //input, output, 79 other internal states from end-start
+        for (int i = 0; i < size; i++) {
+            String bits = Hash.randBits(Hash.BIT_LENGTH);
+            data[i] = Hash.sha1(bits, 1);
+            pw.println(convertToCSV(data[i]));
+            //https://www.baeldung.com/java-csv
+        }
+
+        pw.close();
+    }
+
+    public static String convertToCSV(String[] data) {
+        // special characters are not a worry since it's just hex data
+        return Stream.of(data)
+                .collect(Collectors.joining(","));
+    }
+
+}
--- src/Hash.java
+++ src/Hash.java
@@ -1,116 +1,75 @@
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.Arrays;
 import java.util.Random;
 
 public class Hash
 {
+    static final int BIT_LENGTH = 448 - 8 - 8; // 2 8s to allow for padding, guaranteed only 1 block
+
     public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
-//        TODO: optimize
-
-        MessageDigest md = MessageDigest.getInstance("SHA-1");
-        String bits = stringToBits("hello world");
-        System.out.println(bytesToHex(md.digest(new BigInteger(bits, 2).toByteArray())));
-        System.out.println(sha1(bits)[80]);
-//        System.out.println(checkSha1Impl());
-//        encodeData(1, "./src/data.csv");
-    }
-
-    public static void encodeData(int size, String outFile) throws IOException {
-        //512 bit strings
-        //chance of overlap is so small that it doesn't matter
-        //encoded in hex
-
-        PrintWriter pw = new PrintWriter(new FileWriter(outFile));
-        String[][] data = new String[size][81];
-        //input, output, 79 other internal states from end-start
-        for (int i = 0; i < size; i++) {
-            data[i] = sha1(randBits());
-            //https://www.baeldung.com/java-csv
-        }
-
-        pw.close();
-    }
-
-    public static String randBits() {
-        Random rand = new Random();
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < 512; i++) {
-            sb.append(rand.nextBoolean() ? '0' : '1');
-        }
-        return sb.toString();
-    }
-
-    public static String stringToBits(String str) {
-        return new BigInteger(str.getBytes()).toString(2);
-    }
-
-    private static final char[] HEX_ARRAY = "0123456789abcdef".toCharArray();
-    public static String bytesToHex(byte[] bytes) {
-        char[] hexChars = new char[bytes.length * 2];
-        for (int j = 0; j < bytes.length; j++) {
-            int v = bytes[j] & 0xFF;
-            hexChars[j * 2] = HEX_ARRAY[v >>> 4];
-            hexChars[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
-        }
-        return new String(hexChars);
+        System.out.println(checkSha1Impl());
     }
 
     public static boolean checkSha1Impl() throws NoSuchAlgorithmException {
         MessageDigest md = MessageDigest.getInstance("SHA-1");
-        for (int i = 0; i < 100; i++) {
-            String bits = randBits();
-            if (!sha1(bits)[80].equals(bytesToHex(md.digest(new BigInteger(bits, 2).toByteArray())))) {
+        for (int i = 0; i < 1000; i++) {
+            String bits = //stringToBits(UUID.randomUUID().toString() + UUID.randomUUID().toString() + UUID.randomUUID().toString());
+                            randBits(448-8-8);
+            // randbits must be factor of 8
+            String sha1 = sha1(bits, 1)[80];
+            if (!sha1.equals(bytesToHex(md.digest(new BigInteger(bits, 2).toByteArray())))) {
                 System.out.println(i);
-                System.out.println(sha1(bits)[80]);
-                System.out.println(bytesToHex(md.digest(new BigInteger(bits, 2).toByteArray())));
                 System.out.println(bits);
+                System.out.println(new BigInteger(bits, 2).bitLength());
+                System.out.println(sha1);
+                System.out.println(bytesToHex(md.digest(new BigInteger(bits, 2).toByteArray())));
                 return false;
             }
         }
         return true;
     }
 
-    public static String[] sha1(String bits) {
+    public static String[] sha1(String bits, int blocks) {
         int h0 = 0x67452301;
         int h1 = 0xEFCDAB89;
         int h2 = 0x98BADCFE;
         int h3 = 0x10325476;
         int h4 = 0xC3D2E1F0;
 
-        BigInteger message = new BigInteger(bits, 2);
-        System.out.println(bits.length());
-        System.out.println("hello world".getBytes().length*8);
-        message = message.or(BigInteger.ONE.shiftLeft("hello world".getBytes().length*8));
-        //1 in front to keep 0s
-
-        BigInteger msgTemp = message;
-
-        msgTemp = msgTemp.shiftLeft(1).or(BigInteger.ONE);
-
-        while ((msgTemp.bitLength()-1) % 512 != 448) {
-            msgTemp = msgTemp.shiftLeft(1);
+        if (bits.charAt(0) == '1') {
+            bits = "00000000" + bits;
         }
-        msgTemp = msgTemp.shiftLeft(64).or(BigInteger.valueOf(message.bitLength()+1));
+
+        BigInteger message = new BigInteger(bits, 2);
+        message = message.or(BigInteger.ONE.shiftLeft(bits.length()));
+
+        message = message.shiftLeft(1).or(BigInteger.ONE);
+        message = message.shiftLeft((512 + (449 - message.bitLength()) % 512) % 512);
+        message = message.shiftLeft(64).or(BigInteger.valueOf(bits.length()));
+
+        if ((message.bitLength()-1)/512 < blocks) {
+            throw new RuntimeException("too many blocks for message length");
+        }
+        if (blocks != -1 && (message.bitLength()-1)/512 > blocks) {
+            System.out.println("less blocks than required");
+        }
 
         String[] states = new String[81];
-        states[0] = msgTemp.toString(16);
+        states[0] = message.toString(16);
 
-        for (int i = 0; i < msgTemp.bitLength()/512; i++) {
+        for (int i = 0; i < (blocks == -1 ? (message.bitLength()-1)/512 : blocks); i++) {
             int[] w = new int[80];
             for (int j = 0; j < 16; j++) {
-                int shift = msgTemp.bitLength()-1 - (512*i + 32*j + 32);
+                int shift = message.bitLength()-1 - (512*i + 32*j + 32);
                 BigInteger mask = BigInteger.valueOf(Integer.MAX_VALUE)
-                        .shiftLeft(1).or(BigInteger.ONE) //int max val is 32 bits
+                        .shiftLeft(1).or(BigInteger.ONE)
                         .shiftLeft(shift);
-                w[j] = msgTemp.and(mask).shiftRight(shift).intValue();
+                w[j] = message.and(mask).shiftRight(shift).intValue();
             }
             for (int j = 16; j < 80; j++) {
-                w[j] = leftRotate(w[j-3] ^ w[j-8] ^ w[j-14] ^ w[j-16], 1);
+                w[j] = Integer.rotateLeft(w[j-3] ^ w[j-8] ^ w[j-14] ^ w[j-16], 1);
             }
 
             int a = h0;
@@ -122,7 +81,7 @@ public class Hash
             for (int j = 0; j < 80; j++) {
                 int f = 0; int k = 0;
                 if (j >= 0 && j <= 19) {
-                    f = (b & c) ^ (~b & d);
+                    f = (b & c) | (~b & d);
                     k = 0x5A827999;
                 }
                 else if (j >= 20 && j <= 39) {
@@ -130,7 +89,7 @@ public class Hash
                     k = 0x6ED9EBA1;
                 }
                 else if (j >= 40 && j <= 59) {
-                    f = (b & c) ^ (b & d) ^ (c & d);
+                    f = (b & c) | (b & d) | (c & d);
                     k = 0x8F1BBCDC;
                 }
                 else if (j >= 60 && j <= 79) { //or just plain else
@@ -139,10 +98,10 @@ public class Hash
                 }
 
                 //two's complement, signed/unsigned shouldn't matter for addition
-                int temp = leftRotate(a, 5) + f + e + k + w[j];
+                int temp = Integer.rotateLeft(a, 5) + f + e + k + w[j];
                 e = d;
                 d = c;
-                c = leftRotate(b, 30);
+                c = Integer.rotateLeft(b, 30);
                 b = a;
                 a = temp;
 
@@ -158,10 +117,84 @@ public class Hash
 
         return states;
     }
-    private static int leftRotate(int num, int n) {
-        //int is 32 bits
-        return (num << n) | (num >>> (32 - n));
+
+    public static String sha1Singular(String bits) {
+        int h0 = 0x67452301;
+        int h1 = 0xEFCDAB89;
+        int h2 = 0x98BADCFE;
+        int h3 = 0x10325476;
+        int h4 = 0xC3D2E1F0;
+
+        if (bits.charAt(0) == '1') {
+            bits = "00000000" + bits; //padding
+        }
+
+        BigInteger message = new BigInteger(bits, 2);
+        message = message.or(BigInteger.ONE.shiftLeft(bits.length()));
+        //1 in front to keep 0s
+
+        message = message.shiftLeft(1).or(BigInteger.ONE);
+        message = message.shiftLeft((512 + (449 - message.bitLength()) % 512) % 512);
+        message = message.shiftLeft(64).or(BigInteger.valueOf(bits.length()));
+
+        for (int i = 0; i < (message.bitLength()-1)/512; i++) {
+            int[] w = new int[80];
+            for (int j = 0; j < 16; j++) {
+                int shift = message.bitLength()-1 - (512*i + 32*j + 32);
+                BigInteger mask = BigInteger.valueOf(Integer.MAX_VALUE)
+                        .shiftLeft(1).or(BigInteger.ONE)
+                        .shiftLeft(shift);
+                w[j] = message.and(mask).shiftRight(shift).intValue();
+            }
+            for (int j = 16; j < 80; j++) {
+                w[j] = Integer.rotateLeft(w[j-3] ^ w[j-8] ^ w[j-14] ^ w[j-16], 1);
+            }
+
+            int a = h0;
+            int b = h1;
+            int c = h2;
+            int d = h3;
+            int e = h4;
+
+            for (int j = 0; j < 80; j++) {
+                int f = 0; int k = 0;
+                if (j >= 0 && j <= 19) {
+                    f = (b & c) | (~b & d);
+                    k = 0x5A827999;
+                }
+                else if (j >= 20 && j <= 39) {
+                    f = b ^ c ^ d;
+                    k = 0x6ED9EBA1;
+                }
+                else if (j >= 40 && j <= 59) {
+                    f = (b & c) | (b & d) | (c & d);
+                    k = 0x8F1BBCDC;
+                }
+                else if (j >= 60 && j <= 79) { //or just plain else
+                    f = b ^ c ^ d;
+                    k = 0xCA62C1D6;
+                }
+
+                //two's complement, signed/unsigned shouldn't matter for addition
+                int temp = Integer.rotateLeft(a, 5) + f + e + k + w[j];
+                e = d;
+                d = c;
+                c = Integer.rotateLeft(b, 30);
+                b = a;
+                a = temp;
+            }
+
+            h0 += a;
+            h1 += b;
+            h2 += c;
+            h3 += d;
+            h4 += e;
+        }
+
+        return attach(new int[]{h0, h1, h2, h3, h4});
     }
+
+
     private static String attach(int[] parts) {
         String[] partStr = new String[parts.length];
         for (int i = 0; i < parts.length; i++) {
@@ -182,5 +215,39 @@ public class Hash
             hex.append(Integer.toHexString(Integer.parseInt(bin.substring(i, i+4), 2)));
         }
         return hex.toString();
+    }
+
+
+    public static String randBits(int length) {
+        Random rand = new Random();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            sb.append(rand.nextBoolean() ? '0' : '1');
+        }
+        int traverse = 0;
+        while (sb.substring(traverse, traverse + 8).equals("00000000")) {
+            sb = new StringBuilder(sb.substring(traverse + 8));
+            traverse += 8;
+        } //trimming
+        return sb.toString();
+    }
+
+    public static String stringToBits(String str) {
+        String bits = new BigInteger(str.getBytes()).toString(2);
+        while (bits.length() % 8 != 0) {
+            bits = "0" + bits;
+        }
+        return bits;
+    }
+
+    private static final char[] HEX_ARRAY = "0123456789abcdef".toCharArray();
+    public static String bytesToHex(byte[] bytes) {
+        char[] hexChars = new char[bytes.length * 2];
+        for (int j = 0; j < bytes.length; j++) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = HEX_ARRAY[v >>> 4];
+            hexChars[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
+        }
+        return new String(hexChars);
     }
 }
